71caa94a5d5139baaf8a90bd630be8ff46f3601d
inkymike <mike@codeeverything.com>
2016-06-28 10:34:30 +0000

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Wilson
 
-IBM Watson's very distant cousin
+IBM Watson's very distant cousin.
+
+Wilson is an experiment in artificial neural networks. It's a work in progress, but has been tested to work on simple problems like the OR truth table.
+
+I have also tested "real" datasets such as Iris, but Wilson is much less stable with this for some reason (suggestions welcomed!) :)
+
+Wilson is implemented in NodeJS, but there's no reason you couldn't fairly easily make it work in the browser, or port to another language (for example, Wilson is largely based off of other work written in Python).
+
+### Tests/examples
+
+Run ```$ node test.js```
+
+### Learning the OR truth table
+
+We can learn the OR truth table as follows:
+
+```
+// get wilson
+var wilson = require('./wilson.js')();
+
+// learn OR truth table
+wilson.learn([
+    [1,1],
+    [0,0],
+    [0,1],
+    [1,0]
+], [
+    1,
+    0,
+    1,
+    1
+]);
+
+// test
+wilson.predict([[1,1]], 1); // ~0.97
+wilson.predict([[0,1]], 1); // ~0.97
+wilson.predict([[1,0]], 1); // ~0.97
+wilson.predict([[0,0]], 0); // ~0
+```
+
+## License
+
+MIT
--- test.js
+++ test.js
@@ -1,93 +1,8 @@
 
-// train OR
+// get wilson
 var wilson = require('./wilson.js')();
 
-// learn OR
-wilson.learn([
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-    [1,1,1],
-    [0,1,1],
-    [0,0,1],
-], [
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-    'red',
-    'green',
-    'blue',
-]);
-
-// test
-wilson.predict([[1,1,1]], 'red');
-wilson.predict([[0,1,1]], 'green');
-wilson.predict([[0,0,1]], 'blue');
-// new
-wilson.predict([[1.1,1.1,1.1]], 'red');
-wilson.predict([[0,0.5,0.5]], 'green');
-
-// learn OR
+// learn OR truth table
 wilson.learn([
     [1,1],
     [0,0],
@@ -105,3 +20,89 @@ wilson.predict([[1,1]], 1);
 wilson.predict([[0,1]], 1);
 wilson.predict([[1,0]], 1);
 wilson.predict([[0,0]], 0);
+
+// learn RGB
+wilson.learn([
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+], [
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+    'red',
+    'green',
+    'blue',
+]);
+
+// test - known values
+wilson.predict([[1,1,1]], 'red');
+wilson.predict([[0,1,1]], 'green');
+wilson.predict([[0,0,1]], 'blue');
+
+// test - unknown values
+wilson.predict([[1.1,1.1,1.1]], 'red');
+wilson.predict([[0,0.5,0.5]], 'green');
