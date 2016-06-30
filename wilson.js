115e013dcad2fa2c09a229237acc16489f42b9bb
inkymike <mike@codeeverything.com>
2016-06-30 09:11:41 +0000

    Reset from softmax on output layer

--- wilson.js
+++ wilson.js
@@ -232,13 +232,9 @@ function Wilson(opts) {
         
         // hidden > output
         // multiply the hidden weights by the hidden values and sum the resulting matrix (array)
-        // var sum = hidden.dot(hiddenWeights).map(function (val) {
-        //     return activation(val);
-        // });
-        
-        var sum = hidden.dot(hiddenWeights);
-        
-        sum = softmax(sum);
+        var sum = hidden.dot(hiddenWeights).map(function (val) {
+            return activation(val);
+        });
         
         // > output
         return sum;
