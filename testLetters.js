42bd68f1dcdd4c0af0a4d339244ce161d7734614
inkymike <mike@codeeverything.com>
2016-06-30 15:55:24 +0000

    Added OCR test from Stephen Miller's Mind project

---
+++ testLetters.js
@@ -0,0 +1,96 @@
+
+// get wilson
+var wilson = require('./wilson.js')();
+
+/**
+ * Letters.
+ *
+ * - Imagine these # and . represent black and white pixels.
+ */
+
+var a = character(
+  '.#####.' +
+  '#.....#' +
+  '#.....#' +
+  '#######' +
+  '#.....#' +
+  '#.....#' +
+  '#.....#'
+);
+
+var b = character(
+  '######.' +
+  '#.....#' +
+  '#.....#' +
+  '######.' +
+  '#.....#' +
+  '#.....#' +
+  '######.'
+);
+
+var c = character(
+  '#######' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#......' +
+  '#######'
+);
+
+/**
+ * Learn the letters A through C.
+ */
+
+wilson.learn([
+    a,
+    b,
+    c
+], [
+    'a',
+    'b',
+    'c'
+]);
+
+/**
+ * Predict the letter C, even with a pixel off.
+ */
+
+var result = wilson.predict(character(
+  '.######' +
+  '#......' +
+  '#......' +
+  '#.#....' +
+  '#......' +
+  '##.....' +
+  '######.'
+), 'c');
+
+console.log(result); // ~ 0.5
+
+/**
+ * Turn the # into 1s and . into 0s.
+ */
+
+function character(string) {
+  return string
+    .trim()
+    .split('')
+    .map(integer);
+
+  function integer(symbol) {
+    if ('#' === symbol) return 1;
+    if ('.' === symbol) return 0;
+  }
+}
+
+/**
+ * Map letter to a number.
+ */
+
+function map(letter) {
+  if (letter === 'a') return [ 0.1 ];
+  if (letter === 'b') return [ 0.3 ];
+  if (letter === 'c') return [ 0.5 ];
+  return 0;
+}
